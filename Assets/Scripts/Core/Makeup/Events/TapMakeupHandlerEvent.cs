df0523bf938d600d693db7f507c8f02aea7f5ecf
Alexandr <rfst@mail.ru>
2026-03-24 21:09:59 +0700

    add event

---
+++ Assets/Scripts/Core/Makeup/Events/TapMakeupHandlerEvent.cs
@@ -0,0 +1,14 @@
+﻿using GameEvents;
+
+namespace Core.Makeup.Events
+{
+    public class TapMakeupHandlerEvent : IGameEvent
+    {
+        public readonly MakeupType Type;
+
+        public TapMakeupHandlerEvent(MakeupType type)
+        {
+            Type = type;
+        }
+    }
+}
