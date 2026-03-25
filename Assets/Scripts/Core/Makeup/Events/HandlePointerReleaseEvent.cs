00d45a05ea6776585258b24a30a5aa7eadb823e5
Alexandr <rfst@mail.ru>
2026-03-25 19:47:31 +0700

    create new events

---
+++ Assets/Scripts/Core/Makeup/Events/HandlePointerReleaseEvent.cs
@@ -0,0 +1,15 @@
+﻿using GameEvents;
+using UnityEngine;
+
+namespace Core.Makeup.Events
+{
+    public class HandlePointerReleaseEvent : IGameEvent
+    {
+        public Vector2 Position;
+
+        public HandlePointerReleaseEvent(Vector2 position)
+        {
+            Position = position;
+        }
+    }
+}
--- Assets/Scripts/Core/Makeup/Events/TapMakeupHandlerEvent.cs
+++ Assets/Scripts/Core/Makeup/Events/TapMakeupHandlerEvent.cs
@@ -5,18 +5,10 @@ namespace Core.Makeup.Events
     public class TapMakeupHandlerEvent : IGameEvent
     {
         public readonly MakeupStyle Style;
-        public readonly MakeupType Type;
-        public readonly ColorType Color;
 
-        public TapMakeupHandlerEvent(MakeupType type, ColorType color)
+        public TapMakeupHandlerEvent(MakeupStyle style)
         {
-            Type = type;
-            Color = color;
-            Style = new MakeupStyle()
-            {
-                Color = color,
-                Type =  type
-            };
+            Style = style;
         }
     }
 }
