1876088b21c07ff805764b9a1ebbff5bf121c259
Alexandr <rfst@mail.ru>
2026-03-25 23:07:52 +0700

    create sponge

---
+++ Assets/Scripts/Core/Makeup/Sponge.cs
@@ -0,0 +1,31 @@
+﻿using Core.Makeup.Events;
+using GameEvents;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+namespace Core.Makeup
+{
+    [RequireComponent(typeof(Button))]
+    public class Sponge : MonoBehaviour
+    {
+        [SerializeField] private Button button;
+
+        [Inject] private readonly IGameEventsDispatcher _eventsDispatcher;
+
+        private void Awake()
+        {
+            button.onClick.AddListener(OnSpongeClick);
+        }
+
+        private void OnDestroy()
+        {
+            button.onClick.RemoveListener(OnSpongeClick);
+        }
+
+        private void OnSpongeClick()
+        {
+            _eventsDispatcher.Dispatch(new MakeupEraseEvent());
+        }
+    }
+}
